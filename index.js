985b3322f68b295749c6af761f3ad962b67d6708
Niels Poensgen <niels.poensgen@gmail.com>
2021-03-17 14:09:31 +0100

    first maxpat

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const {Builder, By, Key, until } = require('selenium-webdriver');
 const chrome = require("selenium-webdriver/chrome");
-(async function example() {
-    let options = new chrome.Options();
-    options.addArguments("--disable-notifications");
-    let driver = new Builder()
-    .forBrowser("chrome")
-    .setChromeOptions(options)
-    .build();
-    // let driver = await new Builder().withCapabilities(options.toCapabilities()).forBrowser('chrome').build();
+const Max = require('max-api');
+
+
+let options = new chrome.Options();
+options.addArguments("--disable-notifications");
+let driver = new Builder()
+.forBrowser("chrome")
+.setChromeOptions(options)
+.build();
+
+const setup = async () => {
     try {
-    await driver.get('http://www.reddit.com');
-    // await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('reddit: the front page of the internet'), 3000);
-    driver.executeScript("window.scrollBy(0,1000)");
-    driver.executeScript("window.scrollBy(0,1000)");
-    driver.executeScript("window.scrollBy(0,1000)");
-    } finally {
-    // await driver.quit();
+        await driver.get('http://www.reddit.com');
+        // await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+        await driver.wait(until.titleIs('reddit: the front page of the internet'), 3000);
+        } finally {
+        }
+}
+Max.addHandler("bang", () => {
+    total_height = driver.executeScript("return document.body.scrollHeight");
+    
+    Max.post(total_height);
+    for (let i = 0; i<=100; i++){
+        driver.executeScript("window.scrollBy(0,10)");
     }
-})();
+
+	// driver.xecuteScript("window.scrollBy(0,1000)");
+});
+setup();
